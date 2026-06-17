3f42ab9c752858316de1b6e0a27e7c126c02ce1f
itssyedikramali123-spec <itssyedikramali123@gmail.com>
2026-06-17 23:00:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ st.title("IKRAMs score calculator")
 st.markdown("### *Check Your Results Instantly!*")
 st.markdown("___")
 #lets set up two variable to compare
-my_score=st.number_input("enter your score:",min_value=0,max_value=100,value=90)
+x=st.number_input("enter your score of subject 1:",min_value=0,max_value=100,value=90)
+y=st.number_input("enter your score of subject 2:",min_value=0,max_value=100,value=90)
+z=st.number_input("enter your score of subject 3:",min_value=0,max_value=100,value=90)
+a=st.number_input("enter your score of subject 4:",min_value=0,max_value=100,value=90)
+b=st.number_input("enter your score of subject 5:",min_value=0,max_value=100,value=90)
+f=(a+b+x+y+z)
+my_score=(f/5)
 st.markdown("___")
-passing_score=70
-perfect_score=100
+passing_score=150
+perfect_score=500
 #. greater than (>)
 if my_score>passing_score:
     st.success("yesss YOU have proved yourself you passed")
@@ -18,16 +24,16 @@ if my_score==perfect_score:
 #== is sign og equal and!= is for not equal to
 if my_score!=perfect_score:
     st.write("you have to do more hardwork to acheive 100")
-if my_score>=85:
+if my_score>=450:
     st.balloons()
     st.write("you got an A grade")
-elif my_score>=70:
+elif my_score>=350:
     st.snow()
     st.info("you got B grade")
-elif my_score>=55:
+elif my_score>=250:
     st.snow()
     st.warning("you got C grade")
-elif my_score>=40:
+elif my_score>=150:
     st.info("you got D grade")
 else:
     st.error("you got E grade")
