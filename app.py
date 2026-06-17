c8d4b6d0b49c3dae3329760f0c89787a84bee5a5
itssyedikramali123-spec <itssyedikramali123@gmail.com>
2026-06-17 23:25:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ if my_score==perfect_score:
     st.balloons()
     st.write("you got perfect score you are unbeatable")
 #== is sign og equal and!= is for not equal to
+    if my_score>0:
     st.write(my_score)
 if my_score>=90:
     st.balloons()
