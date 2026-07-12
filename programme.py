aad5e5be83bfcceaa4d6a7d4cae3c109eef4a63c
itssyedikramali123-spec <itssyedikramali123@gmail.com>
2026-07-12 19:08:05 +0530

    Update programme.py

--- programme.py
+++ programme.py
@@ -1,8 +1,10 @@
 import streamlit as st
-st.title(" DISCOUNT & TAX calculator")
+st.title(" Ikram's DISCOUNT & TAX calculator")
 real_prize=st.number_input("Enter your MRP prize of product :",min_value=0,max_value=10000000,value=0)
 discount=st.number_input("Enter your discount percentage :",min_value=0,max_value=100,value=0)
 #real_discount=rc
 rc=real_prize/100*discount
-st.write("your MRP after discount is",real_prize-rc)
+p=real_prize-rc
+st.write("your MRP after discount is",p)
+st.write("Total money you saved",rc)
 
